9dc8c54df2d8873d5ed2643395335771300ec2af
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2021-01-27 09:16:57 +0100

    Improve zsh autocompletion and add syntax highlighting plugin

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -1,15 +1,21 @@
 bindkey -v           # vi key bindings
 export KEYTIMEOUT=1  # reduce timeout between normal and insert mode
 
-fpath=("$ZDOTDIR/themes" "$ZDOTDIR/plugin" $fpath) # add plugins and themes to path
+fpath=("$ZDOTDIR/themes" "$ZDOTDIR/plugins" $fpath) # add plugins and themes to path
 
 source "$ZDOTDIR/aliases.zsh"       # list of aliases
 source "$ZDOTDIR/themes/simple.zsh" # minimalistic terminal prompt
-source "$ZDOTDIR/plugin/fzf.zsh"    # fzf completion
+source "$ZDOTDIR/plugins/fzf.zsh"    # fzf completion
+
+# completion suggestions as yo type
+source "$ZDOTDIR/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+# syntax highlighting
+source "$ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
 _gen_fzf_default_opts               # color theme for fzf
 autoload -Uz cursor.zsh; cursor.zsh # change the cursor based on the vi mode
 
-source "$ZDOTDIR/plugin/completion.zsh" # custom completion configuration
+autoload -Uz completion.zsh; completion.zsh
 autoload -Uz compinit; compinit         # enable completion
 
 ## History command configuration
--- .config/zsh/plugins/completion.zsh
+++ .config/zsh/plugins/completion.zsh
@@ -1,4 +1,5 @@
 zmodload zsh/complist
+
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
@@ -34,68 +35,71 @@ zstyle ':completion:*' expand prefix suffix 
 # Group matches and describe.
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:matches' group 'yes'
-# zstyle ':completion:*:options' description 'yes'
-# zstyle ':completion:*:options' auto-description '%d'
-# zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
-# zstyle ':completion:*:descriptions' format ' %F{green}-- %d --%f'
-# zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-# zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-# zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-# zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
-# zstyle ':completion:*' group-name ''
-# zstyle ':completion:*' verbose yes
-# zstyle ':completion:*' expand prefix suffix 
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{green}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' expand prefix suffix 
 
 # Fuzzy match mistyped completions.
-# zstyle ':completion:*' completer _complete _match _approximate
-# zstyle ':completion:*:match:*' original only
-# zstyle ':completion:*:approximate:*' max-errors 1 numeric
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
 
 # Increase the number of errors based on the length of the typed word. But make
 # sure to cap (at 7) the max-errors to avoid hanging.
-# zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
+zstyle -e ':completion:*:approximate:*'\
+  max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
 
 # Don't complete unavailable commands.
-# zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
 
 # Array completion element sorting.
-# zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
 
 # Directories
-# zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-# zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
-# zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
-# zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
-# zstyle ':completion:*' squeeze-slashes true
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+zstyle ':completion:*:-tilde-:*'\
+  group-order 'named-directories' 'path-directories' 'users' 'expand'
+zstyle ':completion:*' squeeze-slashes true
 
 # Environment Variables
-# zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
+zstyle ':completion::*:(-command-|export):*'\
+  fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
 
 # Populate hostname completion. But allow ignoring custom entries from static
 # */etc/hosts* which might be uninteresting.
 
-# zstyle -e ':completion:*:hosts' hosts 'reply=(
-  # ${=${=${=${${(f)"$(cat {/etc/ssh/ssh_,~/.ssh/}known_hosts(|2)(N) 2> /dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
-  # ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2> /dev/null))"}%%(\#${_etc_host_ignores:+|${(j:|:)~_etc_host_ignores}})*}
-  # ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
-# )'
+zstyle -e ':completion:*:hosts' hosts 'reply=(
+  ${=${=${=${${(f)"$(cat {/etc/ssh/ssh_,~/.ssh/}known_hosts(|2)(N) 2> /dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
+  ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2> /dev/null))"}%%(\#${_etc_host_ignores:+|${(j:|:)~_etc_host_ignores}})*}
+  ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
+)'
 
 # Don't complete uninteresting users...
-# zstyle ':completion:*:*:*:users' ignored-patterns \
-#   adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
-#   dbus distcache docker dovecot fax ftp games gdm gkrellmd gopher \
-#   hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
-#   mailman mailnull mldonkey mysql nagios \
-#   named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
-#   operator pcap postfix postgres privoxy pulse pvm quagga radvd \
-#   rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs '_*'
+zstyle ':completion:*:*:*:users' ignored-patterns \
+  adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
+  dbus distcache docker dovecot fax ftp games gdm gkrellmd gopher \
+  hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
+  mailman mailnull mldonkey mysql nagios \
+  named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
+  operator pcap postfix postgres privoxy pulse pvm quagga radvd \
+  rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs '_*'
 
 # ... unless we really want to.
 # zstyle '*' single-ignored show
 
 # Ignore multiple entries.
 zstyle ':completion:*:(rm|kill|diff|git commit):*' ignore-line other
-# zstyle ':completion:*:rm:*' file-patterns '*:all-files'
+zstyle ':completion:*:rm:*' file-patterns '*:all-files'
 
 # SSH/SCP/RSYNC
 # zstyle ':completion:*:(ssh|scp|rsync):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
@@ -108,17 +112,17 @@ zstyle ':completion:*:(rm|kill|diff|git commit):*' ignore-line other
 # partial completion suggestions
 # zstyle ':completion:*' list-suffixes zstyle
 
-# _comp_options+=(globdots) # With hidden files
+_comp_options+=(globdots) # With hidden files
 
-# setopt AUTO_PUSHD        # Push the current directory visited on the stack.
-# setopt PUSHD_IGNORE_DUPS # Do not store duplicates in the stack.
-# setopt PUSHD_SILENT      # Do not print the directory stack after pushd or popd
+setopt AUTO_PUSHD        # Push the current directory visited on the stack.
+setopt PUSHD_IGNORE_DUPS # Do not store duplicates in the stack.
+setopt PUSHD_SILENT      # Do not print the directory stack after pushd or popd
 
-# zle -C hist-complete complete-word _generic
-# zstyle ':completion:hist-complete:*' completer _history
+zle -C hist-complete complete-word _generic
+zstyle ':completion:hist-complete:*' completer _history
 
-# autoload -Uz history-beginning-search-menu
-# zle -N history-beginning-search-menu
+autoload -Uz history-beginning-search-menu
+zle -N history-beginning-search-menu
 
-# [[ -n "${key[Up]}"   ]] && bindkey -- "${key[Up]}"   up-line-or-beginning-search
-# [[ -n "${key[Down]}" ]] && bindkey -- "${key[Down]}" down-line-or-beginning-search
+[[ -n "${key[Up]}"   ]] && bindkey -- "${key[Up]}"   up-line-or-beginning-search
+[[ -n "${key[Down]}" ]] && bindkey -- "${key[Down]}" down-line-or-beginning-search
